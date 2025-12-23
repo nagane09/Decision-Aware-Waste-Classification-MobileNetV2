6d68a71b7c9f9711db5e51475ef5469f43e4378e
Vaishnavi Nagane <vaishnavinagane986@gmail.com>
2025-12-23 22:01:10 +0530

    Update split_dataset.py

--- split_dataset.py
+++ split_dataset.py
@@ -2,24 +2,20 @@ import os
 import shutil
 import random
 
-# Paths
+
 raw_dir = "dataset/raw"
 dataset_dir = "dataset/dataset"
 
-# Classes to use
 classes = ["cardboard", "glass", "metal", "plastic"]
 
-# Split ratios
 train_ratio = 0.7
 val_ratio = 0.15
 test_ratio = 0.15
 
-# Create folder structure
 for split in ["train", "validation", "test"]:
     for cls in classes:
         os.makedirs(os.path.join(dataset_dir, split, cls), exist_ok=True)
 
-# Function to split
 for cls in classes:
     cls_path = os.path.join(raw_dir, cls)
     images = os.listdir(cls_path)
@@ -33,7 +29,7 @@ for cls in classes:
     val_imgs = images[train_end:val_end]
     test_imgs = images[val_end:]
     
-    # Move files
+
     for img in train_imgs:
         shutil.copy(os.path.join(cls_path, img), os.path.join(dataset_dir, "train", cls, img))
     for img in val_imgs:
